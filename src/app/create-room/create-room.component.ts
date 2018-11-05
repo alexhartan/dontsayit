c53a11b701a4840e084990f3b6732953888eb888
Alex Hartan <alex.hartan@transfer-to.com>
2018-11-06 01:15:26 +0200

    Added first screen - select if you want to create room or join room

---
+++ src/app/create-room/create-room.component.ts
@@ -0,0 +1,40 @@
+import { MessageBusService } from '../message-bus/message-bus.service';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { MockProvider } from '../API/MockProvider';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss']
+})
+export class WelcomeComponent implements OnInit {
+
+  public roomForm: FormGroup;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _mockProvider: MockProvider,
+    private _router: Router,
+    private _mbs: MessageBusService
+  ) {
+
+    this.roomForm = this._formBuilder.group({
+      gameRoom: ['']
+    });
+  }
+
+  ngOnInit() {
+    this._mbs.backgroundProgress.emit(100);
+  }
+
+  connectToRoom() {
+    if (this.roomForm.controls['gameRoom'].valid) {
+      const roomCode = this.roomForm.controls['gameRoom'].value;
+      if (this._mockProvider.send()) {
+        this._router.navigate(['playerJoined', roomCode]);
+      }
+    }
+  }
+}
